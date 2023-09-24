8dbb1dfe4afed82a9cfcb25878cab3a4a2db28e8
Rohith Peddi <rxp190007@utdallas.edu>
2023-09-23 20:48:05 -0500

    Adapting to the dataset

--- paths.py
+++ paths.py
@@ -7,3 +7,4 @@ PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 WEIGHTS_PATH = os.path.join(PROJECT_PATH, "weights")
 CT_PATH = os.path.join(PROJECT_PATH, "Datasets", 'crosstask')
 S3D_PATH = "/user/n.dvornik/Git/S3D_HowTo100M/"  # change this
+
